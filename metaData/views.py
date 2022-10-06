7800e8bbde1feb741ccdc9ff58fc3b28c218a788
YEASEOPARK <lim222ok@hufs.ac.kr>
2022-10-06 23:01:24 +0900

    endItem 게시판 Index 디자인 수정

--- metaData/views.py
+++ metaData/views.py
@@ -7,9 +7,18 @@ from .models import *
 @login_required(login_url='common:login')
 def endItemIndex(request):
     page = request.GET.get('page',1) # 페이지
+    kw = request.GET.get('kw','') #검색어
     endItem_list = Item.objects.filter(Q(is_endItem__exact = True) & Q(user__exact = request.user)).order_by('registration_date')
+
+    if kw:
+        endItem_list = endItem_list.filter(
+            Q(code__icontains= kw) |
+            Q(name__icontains=kw) |
+            Q(note__icontains=kw)
+        ).distinct()
+
     paginator = Paginator(endItem_list, 10) # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'endItem_list' : page_obj}
+    context = {'endItem_list' : page_obj, 'page':page, 'kw':kw}
     return render(request, 'metaData/endItemIndex.html', context)
 
